b26c2f2a37eb2c0836a8e087d7e815c1b04950bf
CarlosDBLopes <144997257+CarlosDBLopes@users.noreply.github.com>
2025-11-13 14:19:36 +0000

    Add files via upload

---
+++ BibliON/includes/nav.php
@@ -0,0 +1,41 @@
+<nav class="navbar navbar-expand-xl py-3 custom-navbar" data-bs-theme="dark">
+    <div class="container-fluid">
+        
+        <a class="navbar-brand" href="index.php">
+            <img src="imgs/imagens/Biblion.png" alt="Logo da Empresa" style="height: 80px;">
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCustomContent" aria-controls="navbarCustomContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse align-items-center" id="navbarCustomContent">
+            
+            <ul class="navbar-nav flex-row justify-content-center justify-content-xl-start">
+                <li class="nav-item">
+                    <a class="nav-link nav-text-link" href="produtos.php">Produtos</a>
+                </li>
+            </ul>
+
+            <div class="d-flex flex-grow-1 justify-content-center px-xl-4 my-3 my-xl-0 search-bar">
+                <form class="d-flex w-100" role="search" style="max-width: 500px;">
+                    <input class="form-control me-2" type="search" placeholder="O que procura?" aria-label="Pesquisar">
+                    <button class="btn btn-outline-secondary" type="submit">
+                        <i class="bi bi-search"></i> </button>
+                </form>
+            </div>
+            
+            <ul class="navbar-nav flex-row justify-content-center justify-content-xl-start icon-links">
+                <li class="nav-item"> <a class="nav-link" href="#" aria-label="Página do Cliente">
+                        <i class="bi bi-person-circle fs-4"></i></a>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav flex-row justify-content-center justify-content-xl-start">
+                <li class="nav-item">
+                    <a class="nav-link nav-text-link" href="#">Sobre Nós</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
